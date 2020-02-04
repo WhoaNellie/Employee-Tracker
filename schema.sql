7f17eecfd118dd2bc086db53c6926f4526dd9a32
Nellie <fabybacenelson@gmail.com>
2020-02-04 12:57:48 -0700

    changed dec to float, changed primary keys

--- schema.sql
+++ schema.sql
@@ -5,23 +5,26 @@ create database employeeDB;
 use employeeDB;
 
 create table employee(
-    id int not null auto_increment primary key,
+    id int not null auto_increment,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id int,
     manager_id int,
-    foreign key (role_id) references role(role_id)
+    -- foreign key (role_id) references role(role_id)
+    primary key(id)
 );
 
 create table role(
-    id int not null auto_increment primary key,
+    id int not null auto_increment,
     title varchar(30) not null,
-    salary dec(6,6) not null,
+    salary float not null,
     department_id int,
-    foreign key (department_id) references department(department_id)
+    -- foreign key (department_id) references department(department_id)
+    primary key(id)
 );
 
 create table department(
-    id int not null auto_increment primary key,
-    name varchar(30) not null
+    id int not null auto_increment,
+    name varchar(30) not null,
+    primary key(id)
 )
